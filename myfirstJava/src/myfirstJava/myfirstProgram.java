8c349b91fa5b77934ada14b9eed44f7eec2864b3
Imjustnelvin <monares360@gmail.com>
2022-02-09 18:27:25 +0800

    add user input

--- myfirstJava/src/myfirstJava/myfirstProgram.java
+++ myfirstJava/src/myfirstJava/myfirstProgram.java
@@ -1,24 +1,24 @@
 package myfirstJava;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class myfirstProgram {
 
-	public static void main(String[] args) {
-		System.out.println("My simple java");
-		System.out.println("Enter your username:");
-		Scanner scanner = new Scanner(System.in);
-		String name = scanner.nextLine();
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc = new Scanner(new File("names.txt"));
+		List<String> names = new ArrayList<String>(); //names = Object of ArrayList
 		
-		System.out.println("Hello "  + name + "!" + " How old are you?");
-		int age = scanner.nextInt();
-		
-		if (age >= 18) {
-			System.out.println("Pwede na makulong :p");
-		} else {
-			System.out.println("Batakapa");
-		} 
-		
-		scanner.close();
-
+		while (sc.hasNextLine()) { //while names.txt has a string
+			names.add(sc.nextLine()); // it will add the string of names.txt to names(ArrayList)
+		}
+	
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println(names.get(i));
+		}
 	}
 
 }
