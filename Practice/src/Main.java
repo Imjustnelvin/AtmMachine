795f581d5e09939bcb00a6e4f29582a61e1f0077
Imjustnelvin <monares360@gmail.com>
2022-02-11 18:18:51 +0800

    added Guessing Game

--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 	
@@ -28,6 +29,32 @@ public class Main {
 		System.out.println("\nRESULT: " + firstNum + " + "+ secondNum + " = " + result +"\n");
 	}
 	
+	public static void generateNum() {
+		Scanner scannerRandom = new Scanner(System.in);
+		Random randomNum = new Random();
+		int randomNumber = randomNum.nextInt(20);
+		boolean success = false;
+		int guess = 0;
+		int numTries = 0;
+		System.out.println("Please guess number 1-20");
+		while(success == false) {
+			System.out.print("Your guess: ");
+			int user_input = scannerRandom.nextInt();
+				
+				if (user_input == randomNumber) {
+					System.out.println("Your guess is correct!");
+					success = true;
+					break;
+				} else if (user_input > 20 || user_input < 1) {
+					System.out.println("Out of range(1-20)");
+				} else if (user_input < randomNumber) {
+					System.out.println("Too low");
+				} else if (user_input > randomNumber) {
+					System.out.println("Too high");
+				}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner scannermain = new Scanner(System.in);
 		//Variables
@@ -42,41 +69,50 @@ public class Main {
 		System.out.print("Please enter your number: ");
 		int userInputNum = scannermain.nextInt();
 		
-			if (userInputNum == 1) {
-					System.out.println("\nYou selected \"Print a FullName\"");
-					printName(); // <-- Calling method
-						System.out.print("Back to menu? (0:Yes|1:No) :");
-							int user_select = scannermain.nextInt();
-							if (user_select == 0) {
+		if (userInputNum == 1) { // Option 1
+				System.out.println("\nYou selected \"Print a FullName\"");
+				printName(); // <-- Calling method
+					System.out.print("Back to menu? (0:Yes|1:No) :");
+					int user_select = scannermain.nextInt();
+						if (user_select == 0) {
 							backMenu = true;
-				} else {
-					System.out.println("Thank you for using my App :)");
-					break;
-				}
-				} else if (userInputNum == 2) {
+						} else {
+							System.out.println("Thank you for using my App :)");
+							break;
+						}
+			} else if (userInputNum == 2) { // Option 2
 					System.out.println("\nYou selected \"Add Two Number\"");
 					addTwo(); // <-- Calling method
 						System.out.print("Back to menu? (0:Yes|1:No): ");
 							int user_select = scannermain.nextInt();
+								if (user_select == 0) {
+									backMenu = true;
+								} else {
+									System.out.println("Thank you for using my App :)");
+									break;
+								}
+			} else if (userInputNum == 3) {
+				System.out.println("\nYou selected \"Guessing Game\"");
+				generateNum();
+					System.out.print("Back to menu? (0:Yes|1:No) :");
+						int user_select = scannermain.nextInt();
 							if (user_select == 0) {
 								backMenu = true;
-				} else {
-					System.out.println("Thank you for using my App :)");
-					break;
-				}
-				}
-				else if (userInputNum > 4 || userInputNum < 1) { // if user select not on option
+							} else {
+								System.out.println("Thank you for using my App :)");
+								break;
+							}
+			} else if (userInputNum > 4 || userInputNum < 1) { // if user select not on option
 					System.out.println("\n######Invalid Input######\n");
 					backMenu = true;
-				} else if (userInputNum == 4) { // if user want to quit
-					System.out.println("-->Kunware nag exit yung app<--");
+			} else if (userInputNum == 4) { // if user want to quit
+					System.out.println("System response: Kunware nag exit yung app");
 					break;
-				}
+			}
 		
 		} while (backMenu = true);
+		
 		scannermain.close();
-		
-		
 	} 
 		
 }
